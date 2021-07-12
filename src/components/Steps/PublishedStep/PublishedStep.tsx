b527dcd145fa2bd1b71de0c894e8d0bf415f3e82
Vitor Leonel <falecom@vitorleonel.com>
2021-07-12 00:01:59 -0300

    action to add another product

--- src/components/Steps/PublishedStep/PublishedStep.tsx
+++ src/components/Steps/PublishedStep/PublishedStep.tsx
@@ -1,6 +1,8 @@
-import { PanelProps } from "@blueprintjs/core";
+import { PanelProps, Button } from "@blueprintjs/core";
 // @ts-ignore
 import Lottie from "react-lottie";
+
+import CategoryStep from "../CategoryStep";
 
 import animationData from './animationData.json'
 import { ViewProps } from "./interfaces";
@@ -15,6 +17,10 @@ const PublishedStep = (props: PanelProps<ViewProps>) => {
     }
   };
 
+  const openPanel = () => props.openPanel({
+    renderPanel: CategoryStep,
+  });
+
   return (
     <section className="steps-item">
       <Lottie options={defaultOptions} height={208} width={208} />
@@ -22,6 +28,8 @@ const PublishedStep = (props: PanelProps<ViewProps>) => {
       <div className="published">
         <h2 className="bp3-heading">Congratulations!</h2>
         <p>You have just published your product <strong>{props.title}</strong>.</p>
+
+        <Button icon="plus" text="Add another product" large onClick={openPanel} />
       </div>
     </section>
   );
